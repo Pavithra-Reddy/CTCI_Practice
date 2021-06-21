f081463559017e52e08715663bea677cca471c17
Pavithra_Reddy <pavithram28@gmail.com>
2021-06-21 10:03:50 +0530

    1 isUnique

---
+++ IsUnique.java
@@ -0,0 +1,38 @@
+/*
+* isUnique - if string has all unique characters
+*/
+
+
+import java.util.*;
+
+class IsUnique{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    //System.out.println("Without Using Additional DS : "+ isUniqueWithoutExtraDataStructure(s));
+    //System.out.println("With Using Additional DS : "+ isUniqueWithExtraDataStructure(s));
+    System.out.println(isUniqueWithExtraDataStructure(s));
+  }
+
+  private static boolean isUniqueWithExtraDataStructure(String s){
+    HashSet<Character> chars = new HashSet<Character>();
+    for(int i = 0; i< s.length();i++){
+      if(chars.contains(s.charAt(i))){
+        return false;
+      }
+      else{
+        chars.add(s.charAt(i));
+      }
+    }
+    return true;
+  }
+
+  private static boolean isUniqueWithoutExtraDataStructure(String s){
+    for(int i = 0; i< s.length()-1;i++){
+      if(s.substring(i+1, s.length()).indexOf(s.charAt(i)) > 0){
+        return false;
+      }
+    }
+    return true;
+  }
+}
