5a1cbca7994f85299908e2de241e4c15dafe04d4
Pavithra_Reddy <pavithram28@gmail.com>
2021-06-24 20:17:14 +0530

    Basic Single Linked List

---
+++ 2 Linked Lists/SinglyLinkedList.java
@@ -0,0 +1,83 @@
+/*
+* SinglyLinkedList - Basic Singly LinkedList Operations
+*/
+
+import java.util.*;
+
+class SinglyLinkedList {
+  Node head;
+
+   class Node{
+    int data;
+    Node next = null;
+
+    public Node(int d){
+      this.data = d;
+    }
+  }
+
+  void appendToTail(int d){
+    Node end = new Node(d);
+    if(this.head == null){
+      this.head = end;
+      return;
+    }
+
+    Node n = this.head;
+    while(n.next != null){
+      n= n.next;
+    }
+    n.next = end;
+  }
+
+  void appendToHead(int d){
+    Node newHead = new Node(d);
+    newHead.next = this.head;
+    this.head = newHead;
+  }
+
+  void delete(int d){
+    Node n = this.head;
+    if(n.data == d){
+      this.head = head.next;
+    }
+    while(n.next != null){
+      if(n.next.data == d){
+        n.next = n.next.next;
+        return;
+      }
+      n = n.next;
+    }
+  }
+
+  void print(){
+    Node n = this.head;
+    if(n == null){
+      System.out.println("List is empty");
+      return;
+    }
+    while(n != null){
+      System.out.print(n.data + " ");
+      n = n.next;
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args){
+    SinglyLinkedList sll = new SinglyLinkedList();
+    sll.print();
+    sll.appendToTail(10);
+    sll.print();
+    sll.appendToTail(20);
+    sll.print();
+    sll.delete(10);
+    sll.print();
+    sll.appendToHead(10);
+    sll.print();
+    sll.appendToHead(0);
+    sll.print();
+    sll.appendToTail(30);
+    sll.print();
+  }
+
+}
