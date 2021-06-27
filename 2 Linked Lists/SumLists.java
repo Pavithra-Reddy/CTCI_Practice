e2d5cb36ecb4d3a08fe4d4f8cd397cd3135a30a9
Pavithra_Reddy <pavithram28@gmail.com>
2021-06-27 20:14:34 +0530

    5 Sum Lists Follow Up

--- 2 Linked Lists/SumLists.java
+++ 2 Linked Lists/SumLists.java
@@ -24,8 +24,29 @@ class SumLists extends SinglyLinkedList{
     System.out.println("Lists before Summation");
     sll1.print();
     sll2.print();
-    System.out.println("Summation Result");
+    System.out.println("Summation Result (Reverse Order)");
     SinglyLinkedList sumList = addListsReverseOrder(sll1.head, sll2.head);
+    sumList.print();
+
+    System.out.println("Summation Result");
+
+    //Appends 0's at beginning to make length same
+    Node n1 = sll1.head;
+    Node n2 = sll2.head;
+    while(n1 != null && n2 != null){
+      n1 = n1.next;
+      n2 = n2.next;
+    }
+    while(n1 != null){
+      sll2.appendToHead(0);
+      n1 = n1.next;
+    }
+    while(n2 != null){
+      sll1.appendToHead(0);
+      n2 = n2.next;
+    }
+
+    sumList = addLists(sll1.head, sll2.head);
     sumList.print();
 
   }
@@ -67,6 +88,43 @@ class SumLists extends SinglyLinkedList{
     }
 
     return sll3;
+  }
+
+
+  private static SinglyLinkedList addLists(Node head1, Node head2){
+    SinglyLinkedList sll3 = new SinglyLinkedList();
+    Node n1 = head1;
+    Node n2 = head2;
+    int carry = 0;
+    int sum = 0;
+
+    sum = n1.data + n2.data + addListsRecursion(n1.next, n2.next, sll3);
+    carry = sum/10;
+    sum = sum%10;
+    sll3.appendToHead(sum);
+
+    if(carry != 0){
+      sll3.appendToHead(carry);
+    }
+    return sll3;
+  }
+
+
+  private static int addListsRecursion(Node head1, Node head2, SinglyLinkedList sll3){
+    Node n1 = head1;
+    Node n2 = head2;
+    int carry = 0;
+    int sum = 0;
+    if(n1 == null || n2 == null){
+      return 0;
+    }
+
+    sum = n1.data + n2.data + addListsRecursion(n1.next, n2.next, sll3);
+    carry = sum/10;
+    sum = sum%10;
+    sll3.appendToHead(sum);
+
+    return carry;
   }
 
 
