7f0ef99b4d9e46668bcf69922c70c3eef7d6659b
Pavithra_Reddy <pavithram28@gmail.com>
2021-06-29 19:39:58 +0530

    Linked List optimized and edge cases covered

--- 2 Linked Lists/SinglyLinkedList.java
+++ 2 Linked Lists/SinglyLinkedList.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 class SinglyLinkedList {
   Node head;
+  Node tail;
 
    class Node{
     int data;
@@ -17,6 +18,17 @@ class SinglyLinkedList {
   }
 
   void appendToTail(int d){
+    Node end = new Node(d);
+    if(this.head == null){
+      this.head = end; //if only one node exist
+    }
+    if(this.tail != null){
+      this.tail.next = end;
+    }
+    this.tail = end;
+  }
+
+  void add(int d){
     Node end = new Node(d);
     if(this.head == null){
       this.head = end;
@@ -34,15 +46,25 @@ class SinglyLinkedList {
     Node newHead = new Node(d);
     newHead.next = this.head;
     this.head = newHead;
+    if(this.tail == null){
+      this.tail = this.head; //if only one node exist
+    }
   }
 
   void delete(int d){
     Node n = this.head;
     if(n.data == d){
+      if(this.head == this.tail){
+        //one element exist;
+        this.tail = null;
+      }
       this.head = head.next;
     }
     while(n.next != null){
       if(n.next.data == d){
+        if(n.next == this.tail){
+          this.tail = n;
+        }
         n.next = n.next.next;
         return;
       }
